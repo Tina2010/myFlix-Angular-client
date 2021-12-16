7466bc38f1c961a38df06b0509a72e8292399072
Tina2010 <t.rittmueller@gmx.de>
2021-12-16 10:50:19 +0100

    [feat] adding dialog with api calls to registration

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-registration-form',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: ''};
+
+  constructor
+    (
+      public fetchApiData: UserRegistrationService,
+      public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+      public snackBar: MatSnackBar
+    ) {}
+
 
   ngOnInit(): void {
   }
 
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      console.log(result)
+      this.dialogRef.close();
+      this.snackBar.open('User registration successful', 'Ok', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open('User registration successful', 'Ok', {
+        duration: 2000
+      });
+    });
+  }
 }
