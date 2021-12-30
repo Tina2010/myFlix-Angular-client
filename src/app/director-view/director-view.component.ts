f32944605d5f96552d699ae16cf37598c1173a0b
Tina2010 <t.rittmueller@gmx.de>
2021-12-30 11:06:19 +0100

    [feat] adding html structure and dialog data

--- src/app/director-view/director-view.component.ts
+++ src/app/director-view/director-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-view',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      name: string;
+      bio: string;
+    }
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
